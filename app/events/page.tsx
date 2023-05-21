873b1ecac6c89ff3e007d899e79c6b1894ab1705
Andrei Bucur <andrei.bucur@medicover.com>
2023-05-21 10:37:13 +0300

    Changed the project's structure and added a dummy class

---
+++ app/events/page.tsx
@@ -0,0 +1,38 @@
+interface TradeOrder {
+  amount: number;
+  tradeOrderType: string;
+}
+
+const fetchData = async (): Promise<TradeOrder[] | null> => {
+  try {
+    const API_URL = "http://localhost:5222/api/TradeOrder";
+    const response = await fetch(API_URL);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+async function Home() {
+
+  const data = await fetchData();
+
+  return (
+    <>
+      <h1>Find in-depth information about Next.js features and API.</h1>
+
+      {data !== null && data.length > 0 ? (
+          data.map((tradeOrder, index) => (
+            <h2 key={index}>
+              Amount: {tradeOrder.amount}, Type: {tradeOrder.tradeOrderType}
+            </h2>
+          ))
+        ) : (
+          <p>No trade orders to display</p>
+        )}
+    </>
+  )
+}
+
+export default Home
